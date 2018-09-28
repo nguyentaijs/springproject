186fa8901c7cca0a4b988ff81a0bef8a1042263c
tai <nguyentaijs@gmail.com>
2018-09-28 14:14:03 +0700

    Test using properties

--- src/main/java/info/nguyentai/configuration/MvcConfig.java
+++ src/main/java/info/nguyentai/configuration/MvcConfig.java
@@ -3,6 +3,7 @@ package info.nguyentai.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.view.JstlView;
 @Configuration
 @EnableWebMvc
 @ComponentScan (basePackages = {"info.nguyentai"})
+@PropertySource ("classpath:config.properties")
 public class MvcConfig extends WebMvcConfigurerAdapter {
 	
 	@Bean
--- src/main/java/info/nguyentai/springproject/controller/HomeController.java
+++ src/main/java/info/nguyentai/springproject/controller/HomeController.java
@@ -1,11 +1,10 @@
 package info.nguyentai.springproject.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
+	@Value ("${system.page}")
+	private String pageTitle;
+	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
@@ -26,13 +28,7 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("pageTitle", pageTitle);
 		
 		return "home";
 	}
